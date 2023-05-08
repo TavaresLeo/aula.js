ec9a876a21b5df795abc80ee163d725ebe13db66
Leonardo Tavaress <106924333+TavaresLeo@users.noreply.github.com>
2023-05-07 22:18:10 -0300

    primeiras funções adicionadas

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById(imagem)
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas!`
+    if (hora>= 0 && hora < 12) {
+        // BOM DIA  
+        img.src = 'nascer.png'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE
+        img.src = 'por-do-sol.png'
+    }
+}
