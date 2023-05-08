b7210c39d647c71572eadf341054f9230ed8f25c
Leonardo Tavaress <106924333+TavaresLeo@users.noreply.github.com>
2023-05-07 23:13:17 -0300

    finalização do projeto

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById(imagem)
+    var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 7
     msg.innerHTML = `Agora são ${hora} horas!`
-    if (hora>= 0 && hora < 12) {
+    if (hora >= 0 && hora < 12) {
         // BOM DIA  
-        img.src = 'nascer.png'
-    } else if (hora >= 12 && hora < 18) {
+        img.src ='imagens/nascer.png'
+        document.body.style.background = '#e2cd9f'
+        document.body.style.innerHTML = 'Bom Dia'
+    } else if (hora >= 12 && hora < 17) {
         //BOA TARDE
-        img.src = 'por-do-sol.png'
+       img.src = 'imagens/por-do-sol.png'
+       document.body.style.background = '#b9846f'
+    } else {
+        //BOA NOITE
+        img.src = 'imagens/foto-noite.png'
+        document.body.style.background = '#515154'
     }
 }
+
